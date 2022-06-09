60b791038cccf8b74ecf875af01dec00ab0d0b12
raushanhvt <raushan.kumar@hexaviewtech.com>
2022-06-09 22:45:25 +0530

    order service completed

---
+++ order-service/src/main/java/com/raushan/orderservice/controller/OrderController.java
@@ -0,0 +1,22 @@
+package com.raushan.orderservice.controller;
+
+import com.raushan.orderservice.dto.OrderRequest;
+import com.raushan.orderservice.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/order")
+public class OrderController {
+
+    private final OrderService orderService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public String placeOrder(@RequestBody OrderRequest orderRequest) {
+        orderService.placeOrder(orderRequest);
+        return "Order Placed Successfully";
+    }
+}
---
+++ order-service/src/main/java/com/raushan/orderservice/service/OrderService.java
@@ -0,0 +1,40 @@
+package com.raushan.orderservice.service;
+
+import com.raushan.orderservice.dto.OrderLineItemsDto;
+import com.raushan.orderservice.dto.OrderRequest;
+import com.raushan.orderservice.model.Order;
+import com.raushan.orderservice.model.OrderLineItems;
+import com.raushan.orderservice.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+
+    public void placeOrder(OrderRequest orderRequest) {
+
+        Order order = new Order();
+        order.setOrderNumber(UUID.randomUUID().toString());
+
+        List<OrderLineItems> objects = orderRequest.getLineItemsDtoList().stream().map(this::mapToDto).toList();
+
+        order.setOrderLineItemsList(objects);
+
+        orderRepository.save(order);
+    }
+
+    private OrderLineItems mapToDto(OrderLineItemsDto orderLineItemsDto) {
+        OrderLineItems orderLineItems = new OrderLineItems();
+        orderLineItems.setPrice(orderLineItemsDto.getPrice());
+        orderLineItems.setQuantity(orderLineItemsDto.getQuantity());
+        orderLineItems.setSkuCode(orderLineItemsDto.getSkuCode());
+
+        return orderLineItems;
+    }
+}
